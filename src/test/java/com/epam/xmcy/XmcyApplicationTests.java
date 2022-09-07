69914eb87ddf06ce97c5ab922008a55c844d8b72
Yahor Litvinchuk <yahor_litvinchuk@epam.com>
2022-09-07 11:55:12 +0300

    Init project

---
+++ src/test/java/com/epam/xmcy/XmcyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.xmcy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmcyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
