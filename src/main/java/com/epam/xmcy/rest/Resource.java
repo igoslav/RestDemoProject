b2512ccde1b367d9690560ce3c26653b6a84d2bc
Yahor Litvinchuk <yahor_litvinchuk@epam.com>
2022-09-07 15:19:44 +0300

    Add basic endpoints

--- src/main/java/com/epam/xmcy/rest/Resource.java
+++ src/main/java/com/epam/xmcy/rest/Resource.java
@@ -1,15 +1,57 @@
 package com.epam.xmcy.rest;
 
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDate;
+
+/**
+ * Main controller to interact with cryptocurrencies data.
+ */
 @Controller
+@RequestMapping("/cryptocurrencies")
 public class Resource {
 
-    @GetMapping("/test")
+    /**
+     * Method returns a descending sorted list of all the cryptos,
+     * based on the normalized range.
+     *
+     * @return list of cryptocurrencies.
+     */
+    @GetMapping
     @ResponseBody
-    public String test() {
+    public String getAllCryptos() {
         return "Hello world!";
+    }
+
+    /**
+     * Method returns statistics for a particular cryptocurrency within range.
+     *
+     * @param crypto cryptocurrency short name (BTC, ETH, etc.).
+     * @param fromDate start date for crypto stats, format dd-MM-yyyy.
+     * @param toDate end date for crypto stats, format dd-MM-yyyy.
+     *
+     * @return cryptocurrency statistic object.
+     */
+    @GetMapping("/{crypto}")
+    @ResponseBody
+    public String getCryptoStatistics(@PathVariable String crypto,
+                                      @RequestParam @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate fromDate,
+                                      @RequestParam @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate toDate) {
+        return "Hello world " + crypto;
+    }
+
+    /**
+     * Method returns cryptocurrency with the highest normalized range for a specific date.
+     *
+     * @param date date to check, format dd-MM-yyyy.
+     *
+     * @return cryptocurrency object.
+     */
+    @GetMapping("/recommendation")
+    @ResponseBody
+    public String getRecommendationByDate(@RequestParam @DateTimeFormat(pattern = "dd-MM-yyyy") LocalDate date) {
+        return "Hello world " + date;
     }
 }
